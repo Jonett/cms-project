cb9568c375a77e231048029f6e94cf467af03d18
Joni <joni.hell@outlook.com>
2020-05-08 07:38:24 +0300

    loadprivatelogs
    returns all log entries from database also includes hashed id.

--- app/models/userlog.php
+++ app/models/userlog.php
@@ -12,10 +12,12 @@
  * @author jonih
  */
 class userlog extends statisticsDatabase {
+
     public function __construct($f3) {
         parent::__construct($f3);
     }
-    public function getMessage($aid){
+
+    public function getMessage($aid) {
         //väliaikainen debug ratkaisu logi rivien tiedoille
         $problems = [
             0 => "Käyttäjä &&&& päivitti profiilia",
@@ -24,21 +26,32 @@ class userlog extends statisticsDatabase {
         ];
         return [$problems[$aid], $aid];
     }
-    public function updateUserLog($message, $userDataArray = NULL){
+
+    public function loadPrivateLog() {
+        $db = $this->db;
+        $logQuery = $db->exec('SELECT * FROM userlog');
+        $hash = new twoWayEncrypt($this->f3->HKEY);
+        foreach($logQuery as $key => $value){
+            $logQuery[$key]['hid'] = $hash->encrypt($value['id']);
+        }
+        return $logQuery;
+    }
+
+    public function updateUserLog($message, $userDataArray = NULL) {
         //message = array jossa 0 = logi teksti, 1 = action id
         $db = $this->db;
         $changes = array();
         $message[0] = str_replace("&&&&", $this->f3->get('SESSION.userEmail'), $message[0]);
-        if($userDataArray != NULL){
-            foreach($userDataArray as $key => $value){
+        if ($userDataArray != NULL) {
+            foreach ($userDataArray as $key => $value) {
                 $changes[$key] = utf8_encode($value);
             }
             $changes = json_encode($changes);
-        }else{
+        } else {
             $changes = json_encode([]);
         }
-        
-        $db->exec('INSERT INTO userlog (action, changes, aid, uid) VALUES ("'.$message[0].'", "'.$changes.'",'.$message[1].','.$this->f3->get('SESSION.uid').')');
-        
+
+        $db->exec('INSERT INTO userlog (action, changes, aid, uid) VALUES ("' . $message[0] . '", "' . $changes . '",' . $message[1] . ',' . $this->f3->get('SESSION.uid') . ')');
     }
+
 }
