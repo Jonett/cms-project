39492adf67b771d56f96428db38a2d2aed2f18e6
Joni <joni.hell@outlook.com>
2020-05-08 07:34:47 +0300

    encryption update
    added option for AES-256-GCM for prod use.

--- app/controllers/twoWayEncrypt.php
+++ app/controllers/twoWayEncrypt.php
@@ -6,26 +6,25 @@
  * @author jonih
  */
 class twoWayEncrypt {
-    private 
-            $securekey, 
-            $iv, 
-            $ivSize, 
-            $method;
+    private $privatekey;
+    private $iv;
+    private $ivSize; 
+    private $method;
     
     function __construct($authKey)
     {
-        //$this->method = "AES-256-CTR";
+        //$this->method = "AES-256-GCM";
         $this->method = "AES-256-CBC";
         $this->ivSize = openssl_cipher_iv_length($this->method);
         $this->iv = openssl_random_pseudo_bytes($this->ivSize);
-        $this->securekey = hash('sha256', $authKey, TRUE);
+        $this->privatekey = hash('sha256', $authKey, TRUE);
     }
     function encrypt($input)
     {
-        return base64_encode($this->iv.openssl_encrypt($input, $this->method, $this->securekey, OPENSSL_RAW_DATA, $this->iv));
+        return base64_encode($this->iv.openssl_encrypt($input, $this->method, $this->privatekey, OPENSSL_RAW_DATA, $this->iv));
     }
     function decrypt($input)
     {
-        return openssl_decrypt(substr(base64_decode($input), $this->ivSize), $this->method, $this->securekey, OPENSSL_RAW_DATA, substr(base64_decode($input), 0, $this->ivSize));
+        return openssl_decrypt(substr(base64_decode($input), $this->ivSize), $this->method, $this->privatekey, OPENSSL_RAW_DATA, substr(base64_decode($input), 0, $this->ivSize));
     }
 }
